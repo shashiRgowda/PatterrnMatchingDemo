7315a86a92706906d721f2b217c25709dbc3e7dd
shashiRgowda <shashikumartr64@gmail.com>
2021-05-20 14:14:06 +0530

    Added:patternvalidationdemo

---
+++ patternvalidationdemo/Pattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegexpPatternValidationDemo
+{
+    class Pattern
+    {
+        public static string REZIX_PINCODE = "^[1-9][0-9]{5}$";
+
+            public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
---
+++ patternvalidationdemo/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace patternvalidationdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns pattern = Patterns();
+            Console.WriteLine(pattern.validatePinCode("4000088"));
+    }
+}
